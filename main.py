6a6b4925551dd2518e79ab311c6f34ced84ee2b7
ch4ot1k <charlesrlemonds@gmail.com>
2020-10-03 14:30:44 -0400

    refactor: format

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import gc
 from ota_updater.wifi_conn import connect_to_wifi
 from machine import reboot
+
 # Creds
 main_dir = 'project'
 wifi_ssid = 'magiceye'
@@ -14,13 +15,13 @@ def check_for_updates():
 	"""
 	Check for any new updates posted to GitHub
 	"""
-	
+
 	# Connect to the WiFI
 	has_wifi = connect_to_wifi(wifi_ssid, wifi_pass)
 	if has_wifi:  # If WiFi is connected
 		from ota_updater.ota_check import OTACheck
-	
-		o = OTACheck(github_url, main_dir=main_dir)  # Init OTA
+
+		o = OTACheck(github_url, main_dir = main_dir)  # Init OTA
 		o.start()  # Check for pending updates
 
 
@@ -28,11 +29,11 @@ def install_updates():
 	"""
 	Download an replace existing files with updated files
 	"""
-	
+
 	# Connect to the WiFI
 	has_wifi = connect_to_wifi(wifi_ssid, wifi_pass)
 	if has_wifi:  # If WiFi is connected
 		from ota_updater.ota_download import OTADownload
-	
-		o = OTADownload(github_url, main_dir=main_dir)  # Init OTA
+
+		o = OTADownload(github_url, main_dir = main_dir)  # Init OTA
 		o.start()
